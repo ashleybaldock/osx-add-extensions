e85ddbd4150f06c6fc2f74046cf5512be8e74347
Timothy Baldock <timothy@baldock.me>
2015-08-29 11:16:39 +0100

    Add dummy app and install/remove scripts

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister Extensions.app
---
+++ remove.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -u Extensions.app
